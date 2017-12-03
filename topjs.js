73b31c6335f61d92acd1b5bf553cb544d876d4e5
Jenny <hzn2017@outlook.com>
2017-12-04 00:40:16 +0800

    Update topjs.js
    changed all original JS codes into JQuery codes.

--- topjs.js
+++ topjs.js
@@ -1,26 +1,22 @@
-var scrollTop;
-var screenWidth;
-var float_menubar = document.getElementById("float_menubar");
-var float_middlePart = document.getElementById("myIntro");
 function toFixate(){
-	float_menubar.className = "menuBarFixated";
-	float_middlePart.className = "middlePartFixated";
-}
+	$("#float_menubar").attr("class", "menuBarFixated");
+	$("#float_middlePart").attr("class", "middlePartFixated");
+};
 function toNotFixate(){
-	float_menubar.className = "menuBar";
-	float_middlePart.className = "middlePart";
-}
-window.onscroll = function(){
-	scrollTop = document.body.scrollTop;
-	screenWidth = screen.width;
-	if(screen.width<401){
-		if(scrollTop > 120){
+	$("#float_menubar").attr("class", "menuBar");
+	$("#float_middlePart").attr("class", "middlePart");
+};
+$(document).scroll(
+	function(){
+	if( $(window).width() <401){
+		if( $(document).scrollTop() > 120){
 			toFixate();
 		}
 		else{toNotFixate();}
 	}
-	else if(scrollTop > 150){
+	else if( $(document).scrollTop() > 150){
 		toFixate();
 	}
 	else{toNotFixate();}
-};
+	}
+);
