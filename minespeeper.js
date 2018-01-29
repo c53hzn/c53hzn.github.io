54d31352c375db9d5c05b332b7dbfd89c8635df1
houzhenni <hzn2017@outlook.com>
2018-01-29 23:00:24 +0800

    created minsweeper game

---
+++ minespeeper.js
@@ -0,0 +1,47 @@
+function minesweeper(matrix) {
+    //给雷区加上边界
+	function toExpand(arr){
+		var line = [];
+		var newArr = [];
+		for(var i = 0; i < arr[0].length + 2; i++){
+			line.push(false);
+		}
+		newArr.push(line);
+		for(var j = 0; j < arr.length; j++){
+			arr[j].unshift(false);
+			arr[j].push(false);
+			newArr.push(arr[j]);
+		}
+		newArr.push(line);
+		return newArr;
+	}
+    //给每一个方格计算数字
+    function toBlur(arr){
+        var sum = 0;
+        for(var i = 0; i < arr.length; i++){
+            for(var j = 0; j < arr[i].length; j++){
+            	if(arr[i][j]){
+            		if(!(i ==1 && j == 1)){//9宫格正中间的不参与计算，第二个的index是1不是222！！！
+            			sum++;
+            		}
+            	}
+            }
+        }
+        return sum;
+    }
+    var expanded = toExpand(matrix);//加了边界的雷区
+    var newMatrix = [];//用来存放标记了数字的雷区
+    for(var i = 0; i < expanded.length - 2; i++){
+        var newLine = [];
+        for(var j = 0; j < expanded[i].length - 2; j++){
+            var line = [];
+            line.push(expanded[i].slice(j,j + 3));//从第j个到第j+3前面的那个: j, j+1, j+2
+            line.push(expanded[i + 1].slice(j,j + 3));
+            line.push(expanded[i + 2].slice(j,j + 3));
+            console.log(line);
+            newLine.push(toBlur(line));
+        }
+        newMatrix.push(newLine);
+    }
+    return newMatrix;
+}
