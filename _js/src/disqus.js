307503bfac254c33406af2e5306ffa07333181fd
houzhenni <hzn2017@outlook.com>
2018-03-25 00:02:19 +0800

    change id for disqus comment div

--- _js/src/disqus.js
+++ _js/src/disqus.js
@@ -27,7 +27,7 @@
 
 export default function loadDisqus() {
   if (document.getElementById('disqus_thread')) {
-    var disq = new iDisqus('comment', {
+    var disq = new iDisqus('disqus_thread', {
         forum: 'houzhenni-com',
         api: 'https://37ed5962-3f77-41a6-bd8e-c7735322cebe.coding.io/api/login.php',
         site: 'https://www.houzhenni.com',
