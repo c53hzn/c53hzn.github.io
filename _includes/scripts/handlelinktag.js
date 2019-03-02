01c89b746c518423a490452b614c20710f436c9a
Jenny <hzn2017@outlook.com>
2019-03-03 02:59:38 +0800

    give up on _blank link

---
+++ _includes/scripts/handlelinktag.js
@@ -0,0 +1,7 @@
+var aTags = document.getElementsByTagName("a");
+var regExForA = /^https:\/\/www\.houzhenni(\.com\/$|\.com\/\d.+)/;
+for (let i = 0; i < aTags.length; i++) {
+	if (!regExForA.test(aTags[i].href) && aTags[i].target != "_blank") {
+	  aTags[i].target = "_blank";
+	}
+}
