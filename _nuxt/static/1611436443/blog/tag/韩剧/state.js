b9d6b8db90d8336b1dd94ce82b298fc53670cafd
HOU Zhenni <hzn2017@outlook.com>
2021-01-24 05:17:43 +0800

    modified path for saving payloads

---
+++ _nuxt/static/1611436443/blog/tag/韩剧/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611436443",layout:"blog",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E9%9F%A9%E5%89%A7",config:{content:{dbHash:"92959c0b"}}};
