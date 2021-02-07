d1974b9c67199047f1cff54e40941f677148b326
c53hzn <hzn2017@outlook.com>
2021-02-07 15:30:20 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/08930cfb405fcbc30cdf2b4fb3a781f238e7ae30

--- _nuxt/static/1612708007/blog/editor-for-my-cms-of-my-nuxt-blog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612708007",layout:"blog",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Feditor-for-my-cms-of-my-nuxt-blog",config:{content:{dbHash:"7cefa254"}}};
---
+++ _nuxt/static/1612711814/blog/script-for-photoshop-bulk-resize-and-watermark/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612711814",layout:"blog",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fscript-for-photoshop-bulk-resize-and-watermark",config:{content:{dbHash:"eb90b03e"}}};
