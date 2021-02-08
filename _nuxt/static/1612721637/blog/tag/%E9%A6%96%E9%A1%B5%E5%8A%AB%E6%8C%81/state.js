7e43c1785eb6286299f74c01805d7180a2740e9a
c53hzn <hzn2017@outlook.com>
2021-02-08 04:51:20 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/2cecbf03a8db97ca67ef147ee28beb990e1a4ec5

--- _nuxt/static/1612721637/blog/tag/%E9%A6%96%E9%A1%B5%E5%8A%AB%E6%8C%81/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612721637",layout:"blog",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E9%A6%96%E9%A1%B5%E5%8A%AB%E6%8C%81",config:{content:{dbHash:"6d2d025d"}}};
---
+++ _nuxt/static/1612759869/blog/roommate-relationship/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612759869",layout:"blog",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Froommate-relationship",config:{content:{dbHash:"441244fd"}}};
