bff2f1afe8d161a0d5fb1ed2ffa3b331e5d4295b
c53hzn <hzn2017@outlook.com>
2021-02-08 05:07:52 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/d6d2817e6bc79dbab811c361d7bf0f687d3fdc64

--- _nuxt/static/1612759869/blog/hao123-issue/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612759869",layout:"blog",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fhao123-issue",config:{content:{dbHash:"441244fd"}}};
