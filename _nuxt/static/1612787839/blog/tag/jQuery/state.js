86f500076f3f7cf0fd0e4c91d62645a2bc7fc48e
c53hzn <hzn2017@outlook.com>
2021-02-09 03:06:10 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/87eea2603ca7bab80923e6ae328b04f212dc6e9c

--- _nuxt/static/1612787839/blog/tag/jQuery/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612787839",layout:"default",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002FjQuery",config:{content:{dbHash:"44754324"}}};
