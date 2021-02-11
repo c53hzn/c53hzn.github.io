bc3d09585df88d062637caed1bd351fcc237e376
c53hzn <hzn2017@outlook.com>
2021-02-11 04:58:57 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/dd6dfb1da1cb0e8b5f9ac808efb6fbc48cd685ee

---
+++ _nuxt/static/1613019527/blog/panda-remit-review/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613019527",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fpanda-remit-review",config:{content:{dbHash:"61cdea70"}}};
