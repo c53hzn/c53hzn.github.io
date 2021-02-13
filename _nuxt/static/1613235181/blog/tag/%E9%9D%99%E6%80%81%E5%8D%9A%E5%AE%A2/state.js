2590542eea38cd9e58c9f9772d3e25d6e405cdea
c53hzn <hzn2017@outlook.com>
2021-02-13 17:16:00 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/7560f5595834e968be06b980c5305253fce26d57

--- _nuxt/static/1613235181/blog/tag/%E9%9D%99%E6%80%81%E5%8D%9A%E5%AE%A2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613235181",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E9%9D%99%E6%80%81%E5%8D%9A%E5%AE%A2",config:{content:{dbHash:"c0d24fae"}}};
--- _nuxt/static/1613235181/blog/tag/Photoshop/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613235181",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002FPhotoshop",config:{content:{dbHash:"c0d24fae"}}};
