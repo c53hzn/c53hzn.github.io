f4c16a19d7612b3e0f2452af2fb45fba72faad3f
c53hzn <hzn2017@outlook.com>
2021-02-14 16:17:00 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/a2ec482c58d629cef7dd718e9ef5830b526d4b84

--- _nuxt/static/1613314914/blog/asmr-recommendations/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613314914",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fasmr-recommendations",config:{content:{dbHash:"e9cfcc5e"}}};
---
+++ _nuxt/static/1613319408/blog/nuxt-blog-generate-files-n-push-to-another-repo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613319408",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fnuxt-blog-generate-files-n-push-to-another-repo",config:{content:{dbHash:"82e0da3a"}}};
