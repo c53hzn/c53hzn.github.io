2289b7fc64e41990d424f1a7ab1e1c67785dbd6c
c53hzn <hzn2017@outlook.com>
2021-02-14 17:30:45 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/6030fba947b5c10bf31d861cdc2c61aa6f46eb87

--- _nuxt/static/1613319408/blog/bag-research/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613319408",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fbag-research",config:{content:{dbHash:"82e0da3a"}}};
