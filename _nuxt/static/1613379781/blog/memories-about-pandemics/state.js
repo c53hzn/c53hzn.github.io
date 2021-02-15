26a069a9e2ab5df783aece1a0fc3cf47a5257cb1
c53hzn <hzn2017@outlook.com>
2021-02-15 09:03:12 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/73d8dcd45e1252ea373cf74464cbc75e389d08d5

---
+++ _nuxt/static/1613379781/blog/memories-about-pandemics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613379781",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fmemories-about-pandemics",config:{content:{dbHash:"946b8112"}}};
---
+++ _nuxt/static/1613379781/blog/tag/%E9%A6%96%E9%A1%B5%E5%8A%AB%E6%8C%81/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613379781",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E9%A6%96%E9%A1%B5%E5%8A%AB%E6%8C%81",config:{content:{dbHash:"946b8112"}}};
