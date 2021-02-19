bd167daaf2c743a06914a44e68b74d656f7ee051
c53hzn <hzn2017@outlook.com>
2021-02-19 20:16:39 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/8e4a5ce2494a820cff329c9ae7d85fd86352bd49

--- _nuxt/static/1613764958/blog/language/%E4%B8%AD%E6%96%87/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613764958",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Flanguage\u002F%E4%B8%AD%E6%96%87",config:{content:{dbHash:"18735a4e"}}};
---
+++ _nuxt/static/1613765786/blog/tag/%E5%A5%B3%E6%9D%83/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613765786",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E5%A5%B3%E6%9D%83",config:{content:{dbHash:"573dcfb2"}}};
