01f259732ca67713db4172126f1f3d739133ec56
c53hzn <hzn2017@outlook.com>
2021-02-20 11:01:07 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/a8857237970b4af7ad7c79e75aee824a571318f7

--- _nuxt/static/1613815753/blog/campus-canteen-memories/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613815753",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fcampus-canteen-memories",config:{content:{dbHash:"67984d9e"}}};
---
+++ _nuxt/static/1613818853/blog/tag/ASMR/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613818853",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002FASMR",config:{content:{dbHash:"e633f70b"}}};
