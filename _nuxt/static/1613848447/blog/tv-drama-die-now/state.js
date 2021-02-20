2a25774a3b0f115455545f79bf848aa9d600b4a5
c53hzn <hzn2017@outlook.com>
2021-02-20 19:14:24 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/e1fcf2925962ff31f366f9d343d79abe053673f7

---
+++ _nuxt/static/1613848447/blog/tv-drama-die-now/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613848447",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftv-drama-die-now",config:{content:{dbHash:"abf79e74"}}};
