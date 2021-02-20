1a893e78d671499b88c912da48ac791be1c2c271
c53hzn <hzn2017@outlook.com>
2021-02-20 12:52:38 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/62011d58713bd5823d6445c969889f44a650a4e9

--- _nuxt/static/1613825011/blog/tag/%E7%94%9F%E6%B4%BB/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613825011",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E7%94%9F%E6%B4%BB",config:{content:{dbHash:"4621db56"}}};
---
+++ _nuxt/static/1613825546/blog/tag/feminism/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613825546",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002Ffeminism",config:{content:{dbHash:"d41577e4"}}};
