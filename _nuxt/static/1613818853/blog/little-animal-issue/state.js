106c5593adb2ce439d8a0936d9a2454ca9ec9867
c53hzn <hzn2017@outlook.com>
2021-02-20 11:40:28 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/c6b752bcc8be9ef038b4e873c917ee8eb11e7818

--- _nuxt/static/1613818853/blog/little-animal-issue/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613818853",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Flittle-animal-issue",config:{content:{dbHash:"e633f70b"}}};
--- _nuxt/static/1613818853/store/template-ebay-desc/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613818853",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fstore\u002Ftemplate-ebay-desc",config:{content:{dbHash:"e633f70b"}}};
