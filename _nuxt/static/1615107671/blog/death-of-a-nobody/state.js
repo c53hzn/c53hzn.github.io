e26685c1d49f5094d47b30d26f59f382676d414b
c53hzn <hzn2017@outlook.com>
2021-03-07 09:01:19 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/082f88b744e6f9a881ba06d2831a8bef937c65df

---
+++ _nuxt/static/1615107671/blog/death-of-a-nobody/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615107671",layout:"post",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fdeath-of-a-nobody",config:{content:{dbHash:"f73f5b7f"}}};
---
+++ _nuxt/static/1615107671/blog/tag/%E5%89%A7%E8%AF%84/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615107671",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E5%89%A7%E8%AF%84",config:{content:{dbHash:"f73f5b7f"}}};
