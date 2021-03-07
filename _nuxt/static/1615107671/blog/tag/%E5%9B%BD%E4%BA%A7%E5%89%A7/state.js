2f6f4355348064916d77d795c588975dcb17de5a
c53hzn <hzn2017@outlook.com>
2021-03-07 10:04:46 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/0c0d4ee1c818ed3a487287a633b6deb08eb62ab4

--- _nuxt/static/1615107671/blog/tag/%E5%9B%BD%E4%BA%A7%E5%89%A7/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615107671",layout:"page",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E5%9B%BD%E4%BA%A7%E5%89%A7",config:{content:{dbHash:"f73f5b7f"}}};
---
+++ _nuxt/static/1615111477/store/calendar-weekly/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615111477",layout:"item",error:null,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fstore\u002Fcalendar-weekly",config:{content:{dbHash:"bc5832fd"}}};
