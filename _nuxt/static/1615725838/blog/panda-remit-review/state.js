ee970a1f7561964b7de97cb58a37c49f6684beaa
c53hzn <hzn2017@outlook.com>
2021-03-14 14:28:19 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/46abb53430b1d24e14da80e7bc232825bd89fbdd

--- _nuxt/static/1615725838/blog/panda-remit-review/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615725838",layout:"post",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fpanda-remit-review",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"51dd76c5"}}}}(null));
---
+++ _nuxt/static/1615732090/store/calendar-100-day/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615732090",layout:"item",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fstore\u002Fcalendar-100-day",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cd325128"}}}}(null));
