a75e7807b069cb15e72baa6280eb4563054cb67b
c53hzn <hzn2017@outlook.com>
2021-03-14 16:51:19 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/7b22da8b1004c7bfa3b23f3dd2108ff5d0fcff0b

--- _nuxt/static/1615732090/blog/script-for-photoshop-bulk-resize-and-watermark/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615732090",layout:"post",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Fscript-for-photoshop-bulk-resize-and-watermark",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cd325128"}}}}(null));
--- _nuxt/static/1615732090/store/calendar-8-page/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615732090",layout:"item",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fstore\u002Fcalendar-8-page",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cd325128"}}}}(null));
