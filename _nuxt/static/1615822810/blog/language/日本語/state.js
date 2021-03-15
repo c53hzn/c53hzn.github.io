5e880326af1b11c6263e8836f8d67dac246941ed
c53hzn <hzn2017@outlook.com>
2021-03-15 17:50:42 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/70413039caad3288a0879b005bf0309338ba5501

--- _nuxt/static/1615822810/blog/language/日本語/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615822810",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Flanguage\u002F%E6%97%A5%E6%9C%AC%E8%AA%9E",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3e42f255"}}}}(null));
---
+++ _nuxt/static/1615830631/blog/tag/女权/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615830631",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E5%A5%B3%E6%9D%83",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ff08f90b"}}}}(null));
