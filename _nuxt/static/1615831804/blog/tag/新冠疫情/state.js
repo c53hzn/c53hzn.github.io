679c8f67c6a41e40fa850920b3b9be2080fcf463
c53hzn <hzn2017@outlook.com>
2021-03-15 18:10:14 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/b1997c1373ba40ccffecd08cfd52a172ab15995e

---
+++ _nuxt/static/1615831804/blog/tag/新冠疫情/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615831804",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E6%96%B0%E5%86%A0%E7%96%AB%E6%83%85",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"518127cd"}}}}(null));
