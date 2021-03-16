6261317aa2bf7dd1a5cf481804fb3d8a242732dd
c53hzn <hzn2017@outlook.com>
2021-03-16 12:25:52 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/1af0206344535556f1fb3d26cc4b55ad2a0c5913

--- _nuxt/static/1615876213/store/calendar-annual/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615876213",layout:"store",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fstore\u002Fcalendar-annual",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e3221b97"}}}}(null));
---
+++ _nuxt/static/1615897541/blog/tag/Excel/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615897541",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002FExcel",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8790208d"}}}}(null));
