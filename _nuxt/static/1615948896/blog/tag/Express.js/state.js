b56d259ecea38fc35404d22adc712fb94239d8b0
c53hzn <hzn2017@outlook.com>
2021-03-17 02:41:48 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/5c1e74dd55d66dba210051ab76b6a18e524d4611

---
+++ _nuxt/static/1615948896/blog/tag/Express.js/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615948896",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002FExpress.js",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7af97954"}}}}(null));
