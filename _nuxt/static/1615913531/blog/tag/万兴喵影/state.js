73beb55e76e05ca8a4319a734c8490e6c986610e
c53hzn <hzn2017@outlook.com>
2021-03-17 01:56:09 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/848d6faa2a02a2149921de0a4aa5f1a2c4ff2641

--- _nuxt/static/1615913531/blog/tag/万兴喵影/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615913531",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E4%B8%87%E5%85%B4%E5%96%B5%E5%BD%B1",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ccf8fb7b"}}}}(null));
