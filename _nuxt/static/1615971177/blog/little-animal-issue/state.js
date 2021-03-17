ed00230363eff1e7af4e07a2d818c00eaaa143e0
c53hzn <hzn2017@outlook.com>
2021-03-17 08:53:09 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/894d133fd348c79968a99bf7d0d1c8dc4adf3855

---
+++ _nuxt/static/1615971177/blog/little-animal-issue/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615971177",layout:"post",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Flittle-animal-issue",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"debdd47f"}}}}(null));
