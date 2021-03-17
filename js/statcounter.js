70e0318faddde3309e41b4538a9f966ce579aa81
c53hzn <hzn2017@outlook.com>
2021-03-17 02:32:59 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/09658c265cefa7ce5f3c990c5cbc72384d41a7ca

--- js/statcounter.js
+++ js/statcounter.js
@@ -1,11 +1,13 @@
-setTimeout(function() {
-	if (window.location.host == "www.houzhenni.com") {
+var loader = setInterval(function () {
+    if(document.readyState !== "complete") return;
+    clearInterval(loader);
+    if (window.location.host == "www.houzhenni.com") {
 		window.sc_project=11716132; 
 		window.sc_invisible=0; 
 		window.sc_security="63c1267e"; 
 		window.scJsHost = "https://";
 		var s = document.createElement("script");
 		s.src = "https://statcounter.com/counter/counter.js";
-		document.querySelector("body").appendChild(s);
+		document.body.appendChild(s);
 	}
-}, 200);
+}, 300);
