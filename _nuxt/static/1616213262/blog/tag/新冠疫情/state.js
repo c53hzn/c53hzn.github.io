8a223a9953fe0c9ec96257f92e543bf2d245ce6f
c53hzn <hzn2017@outlook.com>
2021-03-20 04:20:07 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/77e85911af77542b25555cac9ebda9bbb36df161

--- _nuxt/static/1616213262/blog/tag/新冠疫情/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616213262",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E6%96%B0%E5%86%A0%E7%96%AB%E6%83%85",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2ce65dcc"}}}}(null));
---
+++ _nuxt/static/1616213995/blog/tag/剧评/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616213995",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E5%89%A7%E8%AF%84",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"05503786"}}}}(null));
