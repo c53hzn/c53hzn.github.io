c1a0ac79986185961c6a2c1f34eac693b3241340
c53hzn <hzn2017@outlook.com>
2021-03-25 16:24:00 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/4b4e119cb04645bd7f04362b440217f7188a7bd4

--- _nuxt/static/1616686733/blog/tag/首页劫持/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616686733",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E9%A6%96%E9%A1%B5%E5%8A%AB%E6%8C%81",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3972ba90"}}}}(null));
