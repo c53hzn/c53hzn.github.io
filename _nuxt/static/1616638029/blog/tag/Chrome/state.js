0060e1d8b8b6eb1cf2502ab48ff378f070851caa
c53hzn <hzn2017@outlook.com>
2021-03-25 05:26:44 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/8f2d22e8953e0d19881620eb89c7f900c42ac40f

--- _nuxt/static/1616638029/blog/tag/Chrome/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616638029",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002FChrome",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2b379435"}}}}(null));
