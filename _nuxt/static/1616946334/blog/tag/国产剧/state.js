7a63d148aa0e3a3a52ed5fb704a2571c68090483
c53hzn <hzn2017@outlook.com>
2021-03-28 15:45:51 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/0907226f2df0f18533843a1a7f2412bbd0b0eb5e

---
+++ _nuxt/static/1616946334/blog/tag/国产剧/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616946334",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E5%9B%BD%E4%BA%A7%E5%89%A7",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"85230b46"}}}}(null));
