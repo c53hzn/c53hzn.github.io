b75e76092ba5a80a5ad114207403e6c784323faa
c53hzn <hzn2017@outlook.com>
2021-03-28 15:39:42 +0000

    Update from https://github.com/c53hzn/nuxt-blog/commit/b2de6afa8fd073d84eeb24fad5ef924aa2fc602b

--- _nuxt/static/1616866352/blog/tag/副作用/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616866352",layout:"page",error:a,state:{lang:{val:"Eng",ui:"中文",title:"将UI文本切换成中文"}},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002F%E5%89%AF%E4%BD%9C%E7%94%A8",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5c74aef5"}}}}(null));
